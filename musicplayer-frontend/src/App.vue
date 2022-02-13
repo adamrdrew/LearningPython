3a306d47f81715465c6b16f04df77cf37d61a9f5
Adam Drew <adamrdrew@live.com>
2022-02-13 00:19:04 -0500

    Album UI refinement

--- musicplayer-frontend/src/App.vue
+++ musicplayer-frontend/src/App.vue
@@ -5,8 +5,6 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
 
       <nav>
--- musicplayer-frontend/src/components/cards/AlbumCard.vue
+++ musicplayer-frontend/src/components/cards/AlbumCard.vue
@@ -3,7 +3,8 @@ export default {
   data() {
     return {
       message: "Hey from Adam",
-      albums: []
+      albums: [],
+      TitleMaxLen: 33
     }
   },
   props: {
@@ -21,6 +22,15 @@ export default {
       ChangeIt() {
         this.message = "Did it";
       },
+  },
+  computed: {
+      ShortTitle() {
+          const len = this.album.Title.length;
+          if ( len <= this.TitleMaxLen ) return this.album.Title;
+          const sub = this.album.Title.slice(0, this.TitleMaxLen - 3);
+          const subElipsed = sub + "...";
+          return subElipsed;
+      }
   }
 }
 </script>
@@ -29,8 +39,8 @@ export default {
     <div class="card" style="width: 10em;">
         <img class="card-img-top" v-bind:src="album.ArtSmall" style="width: 10em; height: 10em"/>
         <div class="card-body">
-            <h6 class="card-title">{{album.Title}}</h6>
-            <p class="card-text">{{album.Artist.Title}}</p>
+            <h6 class="card-title">{{ShortTitle}}</h6>
+            <small class="card-text">{{album.Artist.Title}}</small>
         </div>
   </div>
 </template>
